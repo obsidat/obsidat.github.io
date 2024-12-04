4982bca2660d3e874a1556885ab40d18dcc135fa
deliciously-default <181034365+deliciously-default@users.noreply.github.com>
2024-12-04 05:14:28 +0000

    Rename Lorem to Page

--- spa/src/routes.ts
+++ spa/src/routes.ts
@@ -1,11 +1,11 @@
 import Home from './routes/Home.svelte';
-import Lorem from './routes/Lorem.svelte';
+import Page from './routes/Page.svelte';
 import NotFound from './routes/NotFound.svelte';
 
 export default {
     '/': Home,
-    '/private-page/:handle/:rkey/:passphrase': Lorem,
-    '/page/:handle/:rkey': Lorem,
+    '/private-page/:handle/:rkey/:passphrase': Page,
+    '/page/:handle/:rkey': Page,
     // The catch-all route must always be last
     '*': NotFound
 };
