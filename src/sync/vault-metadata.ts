b66ef1eb2dbd1c30c88756647c6e1c6cbc923c7a
deliciously-default <181034365+deliciously-default@users.noreply.github.com>
2024-12-08 16:51:04 -0300

    Fix vaultMetadataCache.files wiping on every tryGetVaultMetadata call

--- src/sync/vault-metadata.ts
+++ src/sync/vault-metadata.ts
@@ -18,7 +18,7 @@ async function tryGetVaultMetadata(agent: XRPCEx, plugin: MyPlugin) {
     let { value: vault, cid } = await agent.tryGetRecord({
         repo: settings.bskyHandle!,
         collection,
-        rkey: plugin.settings.vaultRkey!
+        rkey: settings.vaultRkey!
     });
 
     if (vault) {
@@ -26,17 +26,20 @@ async function tryGetVaultMetadata(agent: XRPCEx, plugin: MyPlugin) {
             await decryptInlineData(vault.metadata, settings.passphrase)
         ) as VaultMetadata;
 
+        console.log('found existing vault record with meta', existingMeta);
+
         // update file data from upstream
         Object.assign(settings.vaultMetadataCache.files, existingMeta.files);
+    } else {
+        console.log('did not find vault record, creating new');
     }
 
     // NB: since all this is async, there could be a race condition where doPush/doPull operate on a different
     // file list than this. this will cause a TypeError. you can simply restart the upload, so i don't care.
     for (const file of app.vault.getAllLoadedFiles()) {
         if (!(file instanceof TFile)) continue;
-        if (file.path in settings.vaultMetadataCache) continue; // TODO incrementally update the metadata instead
 
-        settings.vaultMetadataCache.files[file.path] = {
+        settings.vaultMetadataCache.files[file.path] ??= {
             passphrase: toBase58(randomBytes(20)),
             rkey: tidNow(),
         };
@@ -95,8 +98,11 @@ async function tryFindOrAddVaultToVaults(agent: XRPCEx, plugin: MyPlugin) {
         vaultRkey = existingVault.rkey;
     }
 
+    console.log(`found existing vault`, existingVault);
+
     // no need to update, we already have a vault listed
     if (settings.vaultRkey === vaultRkey && existingVault) {
+        console.log(`not pushing vault as existing vault exists`);
         return vaultRkey;
     }
 
@@ -136,6 +142,7 @@ export async function getVaultMetadata(agent: XRPCEx, plugin: MyPlugin) {
     const { app, settings } = plugin;
 
     settings.vaultRkey ??= await findOrAddVaultToVaults(agent, plugin);
+    await plugin.saveSettings();
 
     let retries = 10;
     while (--retries) {
